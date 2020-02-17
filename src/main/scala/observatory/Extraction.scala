4d30707eeb61222349872b020660b5e86a43caf4
valeriura@gmail.com <valeriura@gmail.com>
2020-02-17 09:49:12 +0100

    milestone 1 draft

--- src/main/scala/observatory/Extraction.scala
+++ src/main/scala/observatory/Extraction.scala
@@ -2,10 +2,26 @@ package observatory
 
 import java.time.LocalDate
 
+import org.apache.log4j.{Level, Logger}
+import org.apache.spark.sql.DataFrame
+
+import scala.io.Source
+
 /**
   * 1st milestone: data extraction
   */
 object Extraction extends ExtractionInterface {
+
+  @transient
+  lazy val spark = org.apache.spark.sql.SparkSession.builder
+    .master("local")
+    .appName("Spark CSV Reader.")
+    .getOrCreate
+
+  import spark.implicits._
+
+  Logger.getLogger("org.apache.spark").setLevel(Level.WARN)
+
 
   /**
     * @param year             Year number
@@ -16,15 +32,34 @@ object Extraction extends ExtractionInterface {
   def locateTemperatures(year: Year,
                          stationsFile: String,
                          temperaturesFile: String): Iterable[(LocalDate, Location, Temperature)] = {
-    ???
+
+    def readDS(path: String): DataFrame = {
+      spark.read
+        .option("header", "false")
+        .option("inferSchema", "true")
+        .csv(Source.fromInputStream(this.getClass.getResourceAsStream(path), "utf-8").getLines().toList.toDS())
+    }
+
+    val stations = readDS(stationsFile)
+    val temperatures = readDS(temperaturesFile)
+
+    stations.filter(station => !station.isNullAt(2) && !station.isNullAt(3)).join(temperatures,
+      stations(stations.columns(0)) <=> temperatures(temperatures.columns(0))
+        && stations(stations.columns(1)) <=> temperatures(temperatures.columns(1)))
+      .collect.toList
+    .map(value => (LocalDate.of(year, value.getInt(6), value.getInt(7)),
+      Location(value.getDouble(2), value.getDouble(3)), Math.round(((value.getDouble(8) - 32.0) * 5.0/9 ) * 100) / 100.0 ))
+
   }
+
 
   /**
     * @param records A sequence containing triplets (date, location, temperature)
     * @return A sequence containing, for each location, the average temperature over the year.
     */
   def locationYearlyAverageRecords(records: Iterable[(LocalDate, Location, Temperature)]): Iterable[(Location, Temperature)] = {
-    ???
+
+    records.groupBy(record => record._2).map(r => (r._1, r._2.map(_._3).sum/r._2.size))
   }
 
 }
--- src/test/scala/observatory/ExtractionTest.scala
+++ src/test/scala/observatory/ExtractionTest.scala
@@ -1,12 +1,44 @@
 package observatory
 
+import java.time.LocalDate
+
 import org.junit.Assert._
-import org.junit.Test
+import org.junit.{Rule, Test}
 
 trait ExtractionTest extends MilestoneSuite {
   private val milestoneTest = namedMilestoneTest("data extraction", 1) _
 
   // Implement tests for the methods of the `Extraction` object
+  // For implicit conversions like converting RDDs to DataFrames
+  @Test def `test locateTemperatures`: Unit = {
+    assert(
+      Extraction.locateTemperatures(2015, "/stations.csv", "/2015_test.csv") ==
+        Seq(
+          (LocalDate.of(2015, 8, 11), Location(37.35, -78.433), 27.3),
+          (LocalDate.of(2015, 12, 6), Location(37.358, -78.438), 0.0),
+          (LocalDate.of(2015, 1, 29), Location(37.358, -78.438), 2.0)
+        ))
+
+  }
+
+  @Test def `test locationYearlyAverageRecords`: Unit = {
+    assert(
+      Extraction.locationYearlyAverageRecords(
+          Seq(
+            (LocalDate.of(2015, 8, 11), Location(37.35, -78.433), 27.3),
+            (LocalDate.of(2015, 12, 6), Location(37.358, -78.438), 0.0),
+            (LocalDate.of(2015, 1, 29), Location(37.358, -78.438), 2.0)
+          )).toSet ==
+        Seq(
+          (Location(37.35, -78.433), 27.3),
+          (Location(37.358, -78.438), 1.0)
+        ).toSet)
+
+  }
+
+
+  @Rule
+  def individualTestTimeout = new org.junit.rules.Timeout(100 * 1000000)
 
 
 }
